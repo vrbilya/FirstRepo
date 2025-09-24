5cae615ae24a073604053cd02536ee2bdc5fe566
vrbilya <ilyavorob2007@gmail.com>
2025-09-24 11:16:55 +0200

    update lab1

--- basics/lab1.cpp
+++ basics/lab1.cpp
@@ -3,20 +3,19 @@
 using namespace std;
 
 int main() {
+	//Создание переменных
 	int a, b;
 	float c;
 	float perimiter;
+	//Ввод переменных
 	cin >> a;
 	cin >> b;
-	if (a > 0 and b > 0) {
-		c = pow(a, 2) + pow(b, 2);
-		c = pow(c, 0.5);
-		perimiter = a + b + c;
-		cout << "гипотенуза: " << c << " " << "периметр: " << perimiter << endl;
-	}
-	else {
-		cout << "Стороны должны быть больше нуля" << endl;
-	}
-	cout << "int: 32 бита, макс.знач(2147483647), мин.знач(-2147483647 - 1)" << endl;
-	cout << "float: 32 бита, макс.знач(3,4e+38), мин.знач(-3,4e-38)";
+	c = pow(a, 2) + pow(b, 2);
+	c = pow(c, 0.5);
+	perimiter = a + b + c;
+	cout << "гипотенуза(float) = " << c << ";  " << "периметр(float) = " << perimiter << endl;
+	cout << "int равен: " << sizeof(int) * 8 << " бита; " << "мин.знач: " << numeric_limits<int>::min() << " макс.знач: " << numeric_limits<int>::max() << endl;
+	cout << "float равен: " << sizeof(float) * 8 << " бита " << "мин.знач: " << numeric_limits<float>::min() << " макс.знач: " << numeric_limits<float>::max() << endl;
+	/*Я выбрал тип int для катетов так как по условию задачи катеты даются в целочисленном формате, а тип данных float я выбрал
+	для гипотенузы так как она может быть не целая и из за этого для периметра я выбрал тоже тип float*/
 }
